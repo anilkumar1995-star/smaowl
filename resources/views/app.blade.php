dbb384783cba36af250d71adabeb1c894dd9aa80
Anil Kumar <kumarglci@gmail.com>
2026-02-24 12:04:15 +0530

    commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,7 @@
         </style>
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
